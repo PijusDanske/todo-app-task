eac62b6e092caf0c515333a736f616b4aa929c6f
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-02 19:11:11 +0300

    feat: create Home page and rename someTasks to tasks

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tasks from "./Tasks";
+
+
+export function Home() {
+  const tasks = [
+    { title: "Wash dishes", done: false },
+    { title: "Read book", done: false },
+    { title: "Get some sleep", done: true },
+  ];
+  return <Tasks tasks={tasks} />;
+}
