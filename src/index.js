264731134e0b436deec56504f05d3a7f008d00de
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-02 19:07:26 +0300

    fix: fix Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import App from "./App";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
 
 import "./styles.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
